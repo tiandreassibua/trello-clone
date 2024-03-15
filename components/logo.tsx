a666f8b42e294a2635839e7a1f9b2e40f3ac2fc5
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-16 01:56:19 +0700

    feat: create an organization based on clerk

--- components/logo.tsx
+++ components/logo.tsx
@@ -15,7 +15,7 @@ export const Logo = () => {
                 <Image src="/logo.svg" alt="Logo" height={30} width={30} />
                 <p
                     className={cn(
-                        "text-lg text-neutral-700 pb-1",
+                        "text-lg text-neutral-700",
                         headingFont.className
                     )}
                 >
