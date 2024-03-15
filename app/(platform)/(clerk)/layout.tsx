8c12e742a448baa4c45e018fe0335fb6ecdf10af
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-16 01:14:48 +0700

    feat: create authentication using clerk

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="h-full flex items-center justify-center bg-slate-100">
+            {children}
+        </div>
+    );
+};
+
+export default ClerkLayout;
